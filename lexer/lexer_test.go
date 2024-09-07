a9af6ae39678557bd545ab6946440c94077fc623
Eros Mendonça <erosmendonca@outlook.com>
2024-09-07 01:32:59 +0100

    monitos

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func TestNextToken(t *testing.T) {
-	input := "=+(){},;"
+	input := `=+(){},;`
 
 	tests := []struct {
 		expectedType    token.TokenType
@@ -22,5 +22,21 @@ func TestNextToken(t *testing.T) {
 		{token.COMMA, ","},
 		{token.SEMICOLON, ";"},
 		{token.EOF, ""},
+	}
+
+	l := New(input)
+
+	for i, tt := range tests {
+		tok := l.NextToken()
+
+		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q",
+				i, tt.expectedType, tok.Type)
+		}
+
+		if tok.Literal != tt.expectedLiteral {
+			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q",
+				i, tt.expectedLiteral, tok.Literal)
+		}
 	}
 }
